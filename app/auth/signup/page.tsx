8723792c4bc254dbc0dc0c4444f9d5b78df2b41e
Lenovo T470 <navidjan.azimi132@gmail.com>
2025-01-09 15:27:11 +0430

    basic Auth Routes for login and signup are done and ready to start the nest js

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,13 @@
+import {Button, Link, Stack, TextField} from "@mui/material";
+import NextLink from "next/link";
+
+export default function signUp() {
+    return (
+        <Stack spacing={2} className="w-full max-w-xs">
+            <TextField label="Email" variant="outlined" type="email"/>
+            <TextField label="Password" variant="outlined" type="passwrod"/>
+            <Button variant='contained'>Sign Up</Button>
+            <Link component={NextLink} href="/auth/login" className="self-center">Login</Link>
+        </Stack>
+    );
+}
